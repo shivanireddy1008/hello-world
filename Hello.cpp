9e481591b0ffb734b623e5b598e65aca3fc3cf91
shivanireddy1008 <se22ucse048@mahindrauniversity.edu.in>
2025-02-04 08:52:47 +0530

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello, " << name << "! Welcome to C++ programming." << std::endl;
+    return 0;
+}
